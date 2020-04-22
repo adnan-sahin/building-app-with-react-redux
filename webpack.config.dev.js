eb96cb6c7dfa3a82985956349ae773dfb6c3ab62
Adnan Sahin <adnansahin1@outlook.com>
2020-04-22 09:27:24 +0300

    apis are added and refactoring

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -23,6 +23,9 @@ module.exports = {
     https: false,
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.API_URL': JSON.stringify('httt://localhost:3001'),
+    }),
     new HtmlWebPackPlugin({
       template: 'src/index.html',
       favicon: 'src/favicon.ico',
